35af890776775560ea90b5c907e1c4ff971f8f76
BrilliantMo <motjiangbrilliantgmail.com>
2025-07-13 03:18:23 +0200

    Initial file upload

---
+++ cmd/api/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	_ "event-booking-system/docs"
+	"event-booking-system/internal/database"
+	"event-booking-system/internal/env"
+	"log"
+
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// @title Go: Event Booking System Rest API
+// @version 1.0
+// @description A rest API in Go using Gin framework
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Enter your bearer token in the format **Bearer &lt;token&gt;**
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	models := database.NewModels(db)
+	app := &application{
+		port:      env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-123456"),
+		models:    models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
+}
